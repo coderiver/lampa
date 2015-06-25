8c7553c16a898b6ca18fc964730ed4b5d6a8bde2
whats0n <mrdvv@mail.ru>
2015-06-25 14:28:51 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -43,31 +43,40 @@ head.ready(function() {
 	});
 
 	//sticky
-	$(function(){ // document ready
-	   if (!!$('.js-sticky').length) { // make sure ".js-sticky" element exists
-	      var el = $('.js-sticky');
-	      var stickyTop = $('.js-sticky').offset().top; // returns number
-	      var footerTop = $('.js-sticky-end').offset().top; // returns number
-	      var stickyHeight = $('.js-sticky').height();
-	      var limit = footerTop - stickyHeight - 10;
-	      $(window).scroll(function(){ // scroll event
-	          var windowTop = $(window).scrollTop(); // returns number
-	            
-	          if (stickyTop-10 < windowTop){
-	             el.css({ position: 'fixed', top: 0 });
-	             el.addClass('is-active');
-	          }
-	          else {
-	             el.css('position','static');
-	             el.removeClass('is-active');
-	          }
-	            
-	          if (limit < windowTop) {
-	          var diff = limit - windowTop;
-	          el.css({top: diff});
-	          }     
-	        });
-	   }
+	$('.page__container').each(function() {
+
+		if ($(this).length > 0) {
+			if ($(this).find('.page__sidebar').outerHeight() < $(this).find('.page__content').outerHeight()) {
+				$(this).find('.js-side-sticky').addClass('js-sticky');
+				$(this).find('.js-side-end').addClass('js-sticky-end');
+				$(function(){ // document ready
+					if (!!$('.js-sticky').length) { // make sure ".js-sticky" element exists
+						var el = $('.js-sticky');
+						var stickyTop = $('.js-sticky').offset().top; // returns number
+						var footerTop = $('.js-sticky-end').offset().top; // returns number
+						var stickyHeight = $('.js-sticky').height();
+						var limit = footerTop - stickyHeight - 10;
+						$(window).scroll(function(){ // scroll event
+							var windowTop = $(window).scrollTop(); // returns number
+						
+							if (stickyTop-10 < windowTop){
+								el.css({ position: 'fixed', top: 0 });
+								el.addClass('is-active');
+							}
+							else {
+								el.css('position','static');
+								el.removeClass('is-active');
+							}
+
+							if (limit < windowTop) {
+								var diff = limit - windowTop;
+								el.css({top: diff});
+							}     
+						});
+					}
+				});
+			};
+		};
 	});
 
 });
