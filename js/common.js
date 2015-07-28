965c91b37edef94bf0ab37e34acfba0fcc2cd6fb
etna-scorpio <etna.scorpio@gmail.com>
2015-07-28 12:31:21 +0300

    popup

--- js/common.js
+++ js/common.js
@@ -1,15 +1,12 @@
 head.ready(function() {
 
 	$(window).resize(function() {
-		var w = "sfgdh";
-
-		console.log(w);
-
 		if($(window).width() < 769) {
 			$('.js-category').removeClass('is-active');
 		}
 	});
 
+	//category block
 	$('.js-click').click(function(e) {
 		e.preventDefault();
 		if($(window).width() > 768){
@@ -38,10 +35,12 @@ head.ready(function() {
 		focusOnSelect: true
 	});
 
+	//fancybox
 	$('.js-fancy').fancybox({
 		openEffect: 'fade'
 	});
 
+	//toggle view from one to two cols
 	$('.js-view').click(function (e) {
 
 		e.preventDefault();
@@ -49,12 +48,37 @@ head.ready(function() {
 		$(this).addClass('is-active');
 
 		if($(this).hasClass('two-col')){
-			$('.article').addClass('is-two-col');
+			$('.js-main').addClass('is-two-col');
 		} else{
-			$('.article').removeClass('is-two-col');
+			$('.js-main').removeClass('is-two-col');
 		}
 	});
 
+	//popup
+	$('.js-info').click(function(){
+		$('.js-popup').addClass('is-active');
+	});
+
+	$('.js-close').click(function () {
+		$('.js-popup').removeClass('is-active');
+	});
+
+	//vertical slider height
+	function verticalHeight(){
+
+		var v = $('.js-vertical');
+		var slide = v.find('.slick-active');
+
+		var vh = slide.outerHeight() * slide.length - 20;
+		v.css({'height': vh});
+
+		$(window).resize(function(){
+			var vh = slide.outerHeight() * slide.length - 20;
+			v.css({'height': vh});
+		});
+	}
+
+	verticalHeight();
 
 
 });
