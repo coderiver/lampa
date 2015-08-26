6406487df875278a0f1826513d72a77d9590b2aa
etna-scorpio <etna.scorpio@gmail.com>
2015-08-26 10:17:40 +0300

    sliders

--- js/common.js
+++ js/common.js
@@ -1,84 +1,48 @@
 head.ready(function() {
+	var articleHeight = $('.article__bl').outerHeight();
 
-	$(window).resize(function() {
-		if($(window).width() < 769) {
-			$('.js-category').removeClass('is-active');
-		}
-	});
-
-	//category block
-	$('.js-click').click(function(e) {
-		e.preventDefault();
-		if($(window).width() > 768){
-			$('.js-category').toggleClass('is-active');
-		}
-	});
-
-	//scroll
-	$('.js-scroll').mCustomScrollbar({
-		scrollInertia: 300
-	});
-
-	//slider
-	$('.js-slidr').slick({
-		fade: true,
-		asNavFor: '.js-vertical'
-	});
-
-	//vertical
-	$('.js-vertical').slick({
-		vertical: true,
-		slidesToShow: 3,
-		verticalSwiping: true,
-		arrows: false,
-		asNavFor: '.js-slidr',
-		focusOnSelect: true
-	});
-
-	//fancybox
-	$('.js-fancy').fancybox({
-		openEffect: 'fade'
-	});
-
-	//toggle view from one to two cols
-	$('.js-view').click(function (e) {
-
-		e.preventDefault();
-		$('.js-view').removeClass('is-active');
-		$(this).addClass('is-active');
-
-		if($(this).hasClass('two-col')){
-			$('.js-main').addClass('is-two-col');
-		} else{
-			$('.js-main').removeClass('is-two-col');
-		}
-	});
-
-	//popup
-	$('.js-info').click(function(){
-		$('.js-popup').addClass('is-active');
-	});
-
-	$('.js-close').click(function () {
-		$('.js-popup').removeClass('is-active');
-	});
-
-	//vertical slider height
-	function verticalHeight(){
-
-		var v = $('.js-vertical');
-		var slide = v.find('.slick-active');
-
-		var vh = slide.outerHeight() * slide.length - 20;
-		v.css({'height': vh});
-
-		$(window).resize(function(){
-			var vh = slide.outerHeight() * slide.length - 20;
-			v.css({'height': vh});
-		});
+	function verticalHeight() {
+		$('.js-vertical').css({height: articleHeight});
 	}
 
 	verticalHeight();
 
+	// scroll
+	$('.js-scroll').mCustomScrollbar({
+		scrollInertia: 300
+	});
 
+	// $('.article__bl').mCustomScrollbar( 'scrollTo', top, {
+	// 	scrollInertia: 300
+	// });
+
+	// category navigation
+	$('.js-navigation').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.js-article',
+		centerMode: true,
+		infinite: true,
+		focusOnSelect: true,
+		variableWidth: true
+	});
+
+	// article
+	$('.js-article').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.js-navigation',
+		infinite: true
+	});
+
+	// popup
+	// $('.js-info').click(function() {
+	// 	$('.js-popup').addClass('is-active');
+	// });
+
+	// $('.js-close').click(function() {
+	// 	$('.js-popup').removeClass('is-active');
+	// });
 });
+
